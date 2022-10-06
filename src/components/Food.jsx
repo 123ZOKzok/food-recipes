980e1f92ec4714c06245245ea87e623d1307b78c
123ZOKzok <user email>
2022-10-06 17:52:11 +0300

    Add more files

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
+import MealItem from "./FoodItem";
+import ReacipeIndex from "./RecipeIndex";
 
 
 const API_URL = 'www.themealdb.com/api/json/v1/1/search.php?f=a';
 
-    const Food = () => {
+const Food = () => {
         const [searchTerm, setSearchTerm] = useState("");
-        const [show,setShow]=useState(false);
-        const [item,setItem]=useState("");
+        const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    searchMeals("a");
+  }, []);
     
     return (
 <div className="food">
