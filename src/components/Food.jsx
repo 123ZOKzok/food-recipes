8ad1c296f763a08714c64b8ac277e4f3e29104c6
123ZOKzok <user email>
2022-10-06 17:27:24 +0300

    Adding event handlers

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 function Food(){
     return (
 <div>
-    
+<div className="heading">
+                <h1>FOOD RECIPES</h1>
+            </div>
+            <div className="search">
+                <input  value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} onKeyPress={searchRecipe} placeholder="Search for recipes"/>
+            </div>
+            <div className="container">
+                {
+                    show ?<MealItem data={item} /> :"Not Found"
+                
+                }
+            </div>
 </div>
     );
 
